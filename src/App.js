d758376f635209973fba7ce0e499742542c8b030
John Leonardo <jdleonardo5@icloud.com>
2019-01-02 13:08:36 -0800

    grommet initializaiton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Box } from 'grommet';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Dosis',
+      weight: 500,
+      size: '20px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grommet theme={theme}>
+        <AppBar>
+          Hello Grommet!
+        </AppBar>
+      </Grommet>
     );
   }
 }
