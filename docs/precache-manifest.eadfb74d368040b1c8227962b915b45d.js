f6edd40ff0c79ebe2910da52bbf9d6b28ef13451
John Leonardo <jdleonardo5@icloud.com>
2019-01-02 20:06:26 -0800

    for gh pages

---
+++ docs/precache-manifest.eadfb74d368040b1c8227962b915b45d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a33064966a4d8ac35f0",
+    "url": "/static/js/main.2a330649.chunk.js"
+  },
+  {
+    "revision": "5f7f65cd8fd5897db59a",
+    "url": "/static/js/1.5f7f65cd.chunk.js"
+  },
+  {
+    "revision": "72e0fe10203ee2d28598b6a4cce57c23",
+    "url": "/index.html"
+  }
+];
